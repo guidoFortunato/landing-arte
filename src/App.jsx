e04cd766fcd26d6a0ae6a71bb192ea898efc8292
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-02-27 20:18:16 -0300

    inicio proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Mi App</h1>
+      {/* <img src={reactLogo}   alt="" /> */}
+    </div>
+  )
+}
+
+export default App
